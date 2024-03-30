6ba2d7e0c3fa732b2f970a504438d9375173266b
Media7834 <165357466+Media7834@users.noreply.github.com>
2024-03-30 21:45:02 +0900

    Update deploy-lolipop.sh

--- deploy-lolipop.sh
+++ deploy-lolipop.sh
@@ -2,20 +2,20 @@
 # デプロイ時にサーバー上で実行されるスクリプト
 
 # vendor以下をインストールして autoloadファイルを最適化
-/usr/local/php8.1/bin/php ../../composer.phar install --no-dev
+/usr/local/php/8.1/bin/php ../../composer.phar install --no-dev
 
 # 環境設定をコピー
 cp .env.lolipop .env
 
 
 # /configの設定情報を1ファイルにまとめておく
-/usr/local/php8.1/bin/php artisan config:cache
+/usr/local/php/8.1/bin/php artisan config:cache
 
 # route情報をまとめておく ※CLOSUREがあると使用できないのでとりまコメントアウト……
-/usr/local/php8.1/bin/php artisan route:cache
+/usr/local/php/8.1/bin/php artisan route:cache
 
 # viewキャッシュをクリア
-/usr/local/php8.1/bin/php artisan view:clear
+/usr/local/php/8.1/bin/php artisan view:clear
 
 # ログをクリア
 rm -f storage/logs/*.log
